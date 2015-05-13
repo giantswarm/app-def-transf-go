931507eabbc3aaecd81499c99b9fec2a0e78f134
Tim Schindler <tim@giantswarm.io>
2015-05-13 13:33:49 +0000

    comments

--- deftypechecker.go
+++ deftypechecker.go
@@ -4,7 +4,15 @@ import (
 	"math"
 )
 
+// defTypeChecker implementations are supposed to detect a certain app
+// definition.
 type defTypeChecker interface {
+	// Parse receives the user given byte slice representing the app definition
+	// to parse detect. It returns its own DefType in any case. The second return
+	// value is a probability that indicates how likely it is to have its own
+	// definition type detected. When returning 0.0 there is no chance that the
+	// given app definition is detected. When 100.0 is returned there is
+	// definitly the given app definition detected.
 	Parse(b []byte) (DefType, float64)
 }
 
